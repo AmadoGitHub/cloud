40942c59e907bf168ed5ec6d8789267427bc1c3b
zhaojiawen <wzhaojiawen@163.com>
2019-08-05 20:27:53 +0800

    cloud

---
+++ springboot-domain/src/main/java/com/springboot/domain/SpringbootDomainApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDomainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDomainApplication.class, args);
+    }
+
+}
